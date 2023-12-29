aa81a7bec35d79a835d15112065fd48f866572e1
Kbgjtn <Kambingjan31@gmail.com>
2023-12-29 14:38:42 +0000

    chore: cover test util parser

---
+++ util/parser_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type ExampleStruct struct {
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
+
+func TestParseRequestBodyJSON(t *testing.T) {
+	requestBody := `{"name": "example", "value": 42}`
+	req, err := http.NewRequest("POST", "/test", bytes.NewBufferString(requestBody))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	var data ExampleStruct
+	err = ParseRequestBody(req, &data)
+	assert.NoError(t, err)
+
+	expectedData := ExampleStruct{Name: "example", Value: 42}
+	assert.Equal(t, expectedData, data)
+}
